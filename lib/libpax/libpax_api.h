47a3859c2df3e7b502b0007e7135fd2b15ffe564
Christian Pacher <g.thb@pacher.com>
2021-09-17 09:48:18 +0200

    Update from ms to s in comment

--- lib/libpax/libpax_api.h
+++ lib/libpax/libpax_api.h
@@ -51,9 +51,9 @@ struct count_payload_t {
 
 /**
  *   Must be called before use of the lib. Initialze a callback the payload paxcount is written back too.
- *   @param[in] callback Callback which is called every pax_report_interval to inform on current pax
+ *   @param[in] callback Callback which is called every pax_report_interval_sec to inform on current pax
  *   @param[out] memory location for pax count. Updated directly before callback is called
- *   @param[in] pax_report_interval defines interval in ms between a pax count callback.
+ *   @param[in] pax_report_interval_sec defines interval in s between a pax count callback.
  *   @param[in] countermode avalible modes TBD
 */
 int libpax_counter_init(void (*callback)(void), struct count_payload_t* current_count, uint16_t pax_report_interval_sec, int countermode);
